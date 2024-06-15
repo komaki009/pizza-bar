6e8f38a8da61e681b27f5709bfa6b000216f38ce
Komaki <zhfa13z@gmail.com>
2024-06-15 18:05:30 +0800

    create footer and layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home/Home"
 import Cart from "./pages/Cart/Cart"
 import Navbar from "./component/Navbar/Navbar"
 import FoodDetail from "./pages/FoodDetail/FoodDetail"
+import Footer from "./component/Footer/Footer"
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Route path="/cart" element={<Cart />} />
         <Route path="food/:id" element={<FoodDetail />} />
       </Routes>
+      <Footer />
     </div>
   )
 }
