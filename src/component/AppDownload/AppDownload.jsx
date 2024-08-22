c7f3312e89147ca8e72b6aaac31927561002b954
Komaki <zhfa13z@gmail.com>
2024-08-22 19:55:32 +0800

    Create AppDownload

---
+++ src/component/AppDownload/AppDownload.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import "./AppDownload.css"
+import appStore from "../../assets/app_store.png"
+import playStore from "../../assets/play_store.png"
+
+const AppDownload = () => {
+  return (
+    <div className="app-download">
+      <p>App 下載</p>
+      <div className="app-download-platforms">
+        <img src={appStore} alt="" />
+        <img src={playStore} alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default AppDownload
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from "react"
 import "./Home.css"
 import Header from "../../component/Header/Header"
 import FoodList from "../../component/FoodList/FoodList"
+import AppDownload from "../../component/AppDownload/AppDownload"
 
 const Home = () => {
   return (
     <div>
       <Header />
       <FoodList />
+      <AppDownload />
     </div>
   )
 }
